0821f83ead678ff05dc646fc2c74e2654bb5b1bd
Daniel Le <daniel.le36@hotmail.com>
2020-07-30 12:58:14 -0400

    Create Map component for displaying the map

---
+++ client/src/components/map.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+
+const Maps = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiZmVlbHNicmVhZG1hbiIsImEiOiJja2Q3ejJyeTMwNXNwMnlwbWVpMm9qMWRzIn0.9QWRsTV_8GVh1-i63Nu3Qg",
+});
+class Map extends Component {
+  render() {
+    return (
+      <div>
+        <Maps
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "100vh",
+            width: "100vw",
+          }}
+        >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}
+          >
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+          </Layer>
+        </Maps>
+        ;
+      </div>
+    );
+  }
+}
+
+export default Map;
