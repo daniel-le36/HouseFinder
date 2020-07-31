d9534644da1b64494066c99805362f2400e9f873
Daniel Le <daniel.le36@hotmail.com>
2020-07-31 16:03:54 -0400

    Add dropdown for choosing preferences

--- client/src/components/preferences.jsx
+++ client/src/components/preferences.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { List, Icon } from "semantic-ui-react";
+import { List, Dropdown } from "semantic-ui-react";
 class Preferences extends Component {
   state = {
     housePrefs: [
@@ -7,9 +7,21 @@ class Preferences extends Component {
       { id: 2, factor: "Churches" },
       { id: 3, factor: "Grocery Stores" },
     ],
+    selectedPrefs: [],
   };
-  ShiftItem = (id, direction) => {
+
+  // Select a preference from the dropdown and add to list
+  SelectPref = (event, { value }) => {
     const preferences = [...this.state.housePrefs];
+    const item = preferences.filter((i) => i.id === value)[0];
+    const selectedPrefs = [...this.state.selectedPrefs];
+    selectedPrefs.push(item);
+    this.setState({ selectedPrefs: selectedPrefs });
+  };
+
+  // Shift a preference to higher or lower priority
+  ShiftItem = (id, direction) => {
+    const preferences = [...this.state.selectedPrefs];
     const position = preferences.findIndex((i) => i.id === id);
     if (
       (position < preferences.length - 1 && direction === 1) ||
@@ -18,15 +30,46 @@ class Preferences extends Component {
       const item = preferences[position]; // save item for later
       const newPrefs = preferences.filter((i) => i.id !== id); // remove item from array
       newPrefs.splice(position + direction, 0, item);
-      this.setState({ housePrefs: newPrefs });
+      this.setState({ selectedPrefs: newPrefs });
     }
+  };
+
+  // Remove a selected preference from the list
+  RemovePref = (id) => {
+    const preferences = [...this.state.selectedPrefs];
+    const filtered = preferences.filter((pref) => pref.id !== id);
+    this.setState({ selectedPrefs: filtered });
   };
 
   render() {
     return (
       <div id="PreferencesContainer">
+        {/* Dropdown for adding preferences*/}
+        <Dropdown
+          placeholder="Select Preferences"
+          fluid
+          selection
+          selectOnNavigation={false}
+          selectOnBlur={false}
+          onChange={this.SelectPref}
+          options={this.state.housePrefs
+            .filter(
+              (pref) =>
+                !this.state.selectedPrefs.some(
+                  (selPref) => pref.id === selPref.id
+                )
+            )
+            .map((pref, index) => {
+              return {
+                text: pref.factor,
+                value: pref.id,
+                key: index,
+              };
+            })}
+        />
+        {/* List of selected preferences */}
         <List>
-          {this.state.housePrefs.map((pref, index) => (
+          {this.state.selectedPrefs.map((pref, index) => (
             <List.Item key={pref.id} style={{ display: "flex" }}>
               <List.Content>{pref.factor}</List.Content>
               <List.Icon
@@ -36,6 +79,10 @@ class Preferences extends Component {
               <List.Icon
                 name="chevron down"
                 onClick={() => this.ShiftItem(pref.id, 1)}
+              />
+              <List.Icon
+                name="cancel"
+                onClick={() => this.RemovePref(pref.id)}
               />
             </List.Item>
           ))}
