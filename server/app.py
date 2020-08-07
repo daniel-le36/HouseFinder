29945d8c1564c241b3f9a4dbebb3cee25cf1a957
Daniel Le <daniel.le36@hotmail.com>
2020-08-07 12:04:55 -0400

    Remove unused imports

--- server/app.py
+++ server/app.py
@@ -1,37 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import json
-import geopy
-import operator
-from scipy import spatial
-import geopy.distance
 import sqlite3
-
-class Neighbourhood():
-    def __init__(self,coords):
-        self.coords = coords
-        self.coordTree = spatial.cKDTree(coords)
-        self.score = 0
-
-# Create a KD tree for each neighbourhood
-def createTrees():
-    NeighbourhoodList = []
-    geo = json.load(open('vaughan.json', encoding='utf-8'))
-    for polygon in geo['coordinates']:
-        newNeighbourhood = Neighbourhood(polygon)
-        NeighbourhoodList.append(newNeighbourhood)
-    return NeighbourhoodList
-
-# Get the locations for the specified category
-def getLocations(pref):
-    locations = json.load(open('locations.json', encoding='utf-8'))
-    locations = [[location['longitude'],location['latitude']] for location in locations['locations'] if location['category'] == pref]
-    return locations
-
-# Calculate the score for a neighbourhood based on its distance and preference ranking
-def calculateScore(distance,position):
-    score = (1 / distance) * (1 / (position+1))
-    return score
 
 # Create a geojson for the neighbourhoods
 def createGeoJson(coordList):
@@ -113,4 +83,3 @@ def getHouses():
     
     # Return results as geojson
     return {'neighbourhoods':createGeoJson(coordDict.values())}
-app.run()
